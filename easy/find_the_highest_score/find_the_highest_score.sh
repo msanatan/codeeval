ad03d0428356ec30d4b262b4e8003d1dcd845ba5
Marcus Sanatan <msanatan@gmail.com>
2015-11-19 15:54:06 -0400

    Close, only prints first max value though

--- easy/find_the_highest_score/find_the_highest_score.sh
+++ easy/find_the_highest_score/find_the_highest_score.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
+MAX=( -10000 -10000 -10000 )
 OLD_IFS="$IFS"
-while read -r line; do
+while read -r line;
+do
   IFS="|"
-  ROWS=($line)
-  for row in ${ROWS[@]}
+  ROWS=$line
+  for row in ${ROWS[@]};
   do
-    echo $row
+    IFS=" "
+    ROW_ARR=($row)
+    for i in ${!ROW_ARR[@]};
+    do
+      if [ "${ROW_ARR[$i]}" -ge "${MAX[$i]}" ]
+      then
+        MAX[$i]=${ROW_ARR[$i]}
+      fi
+    done
   done
+  echo ${MAX}
 done < $1
 IFS="$OLD_IFS"
