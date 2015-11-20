1e2d6a167d877c2a869f5a659475b291dcbf7c49
Marcus Sanatan <msanatan@gmail.com>
2015-11-19 21:01:40 -0400

    Complete :)

--- easy/find_the_highest_score/find_the_highest_score.sh
+++ easy/find_the_highest_score/find_the_highest_score.sh
@@ -4,17 +4,20 @@ while read -r line;
 do
   IFS="|"
   ROWS=$line
-
   MAX=()
-  for (( i=0; i<3; i++));
-  do
-    MAX+=(-100000)
-  done
 
   for row in ${ROWS[@]};
   do
     IFS=" "
     ROW_ARR=($row)
+
+    if [ "${#MAX[@]}" -lt "${#ROW_ARR[@]}" ]
+    then
+      for (( i=0; i<${#ROW_ARR[@]}; i++));
+      do
+        MAX+=(-100000)
+      done
+    fi
 
     for i in ${!ROW_ARR[@]};
     do
